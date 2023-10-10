eadbb0a5af98fe21d72c6da09957d8ccf50c8fa9
VDani3 <villacarmona.dani@gmail.com>
2023-10-10 20:58:39 +0200

    Actualizar a version maven

---
+++ src/main/java/com/project/ControllerVista1.java
@@ -0,0 +1,27 @@
+package com.project;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+
+public class ControllerVista1 {
+    
+    @FXML
+    Button vista1 = new Button();
+
+    @FXML
+    Button action1 = new Button();
+
+    
+
+
+}
---
+++ src/main/java/com/project/Main.java
@@ -0,0 +1,52 @@
+package com.project;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        UtilsViews.parentContainer.setStyle("-fx-font: 14 arial;");
+        UtilsViews.addView(getClass(), "Normal", "/assets/view0.fxml");
+
+        Scene scene = new Scene(UtilsViews.parentContainer);
+        
+        // Listen to window width changes
+        scene.widthProperty().addListener((ChangeListener<? super Number>) new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldWidth, Number newWidth) {
+                _setLayout();
+            }
+        });
+
+        stage.setScene(scene);
+        stage.setTitle("Practica 2"); 
+        stage.setResizable(false);
+        stage.show();
+
+        // Add icon only if not Mac
+        if (!System.getProperty("os.name").contains("Mac")) {
+            Image icon = new Image("file:assets/icons/icon.png");
+            stage.getIcons().add(icon);
+        }
+    }
+
+    private void _setLayout() {
+        UtilsViews.setView("Normal");
+    }
+}
